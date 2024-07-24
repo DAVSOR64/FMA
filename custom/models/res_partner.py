e343e58b139bbd76818079debfbb218ca1cbacec
DAVSOR64 <newdsa64@gmail.com>
2024-07-24 16:41:17 +0000

    UpdateInvoice

--- custom/models/res_partner.py
+++ custom/models/res_partner.py
@@ -3,7 +3,21 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    x_studio_mode_de_rglement_1 = fields.Char(string="Mode de Règlement")
+    x_studio_mode_de_rglement_1 = fields.Selection(
+        [
+            ('ESPECES','ESPECES'),
+            ('CHEQUE BANCAIRE','CHEQUE BANCAIRE'),
+            ('VIREMENT BANCAIRE','VIREMENT BANCAIRE'),
+            ('L.C.R. DIRECTE','L.C.R. DIRECTE'),
+            ('L.C.R. A L ACCEPTATION','L.C.R. A L ACCEPTATION'),
+            ('PRELEVEMENT','PRELEVEMENT'),
+            ('L.C.R. MAGENTIQUE','L.C.R. MAGENTIQUE'),
+            ('BOR','BOR'),
+            ('CARTE BANCAIRE','CARTE BANCAIRE'),
+            ('CREDIT DOCUMENTAIRE','CREDIT DOCUMENTAIRE'),
+        ],
+        string="Mode de Règlement",
+    default = 'VIREMENT BANCAIRE',)
     
     def _prepare_order(self):
         order_vals = super(ResPartner, self)._prepare_order()
