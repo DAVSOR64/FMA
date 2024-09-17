8e5268fe9bd58251368ce687d7f05af4ea2bfd5c
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2024-09-17 07:32:28 +0200

    Update sale_order.py

--- fma_sale_order_custom/models/sale_order.py
+++ fma_sale_order_custom/models/sale_order.py
@@ -6,18 +6,22 @@ from odoo import fields, models
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+      
+# Init date validation devis
 
     def action_validation(self):
         for order in self:
             order.state = 'validated'
             order.x_studio_date_de_la_commande = fields.datetime.today()
             order.so_date_devis_valide = fields.datetime.today()
-
+            
+ # Init date BPE lors de la confirmation du devis
     def action_confirm(self):
         for order in self:
             order.so_date_bpe = fields.datetime.today()
         return super().action_confirm()
-
+        
+ # Init date de modification devis
     def action_quotation_send(self):
         for order in self:
             order.so_date_de_modification_devis =fields.Date.today()
