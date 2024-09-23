9a8086da8a3ea4a6acdf228a9a58c4da2c6ac3a5
Pratima Gupta <pgu@odoo.com>
2024-09-23 15:27:15 +0530

    [IMP] added module for functionality

--- fma_sale_order_custom/models/sale_order.py
+++ fma_sale_order_custom/models/sale_order.py
@@ -10,8 +10,8 @@ class SaleOrder(models.Model):
     state = fields.Selection(selection_add=[
         ('validated', 'Validated')
     ])
-    
-    date_bpe = fields.Date(string="Date BPE") 
+
+    date_bpe = fields.Date(string="Date BPE")
 
     # Init date validation devis
     def action_validation(self):
@@ -19,17 +19,17 @@ class SaleOrder(models.Model):
             order.state = 'validated'
             order.x_studio_date_de_la_commande = fields.datetime.today()
             order.so_date_devis_valide = fields.datetime.today()
-            
+
     # Init date BPE lors de la confirmation du devis
     def action_confirm(self):
         for order in self:
             order.so_date_bpe = fields.datetime.today()
         return super().action_confirm()
-    
-    # Init date bon pour fab
+
+    # Init date ARC validé
     def action_confirm(self):
         for order in self:
-            order.so_date_bon_pour_fab = fields.datetime.today()
+            order.so_date_ARC_valide = fields.datetime.today()
         return super().action_confirm()
 
     # Init date fin de production réel
@@ -37,7 +37,7 @@ class SaleOrder(models.Model):
         for order in self:
             order.so_date_de_fin_de_production_reel = fields.Date.today()
         return super().button_mark_done()
-        
+
     # Init date de modification devis
     def action_quotation_send(self):
         for order in self:
@@ -145,17 +145,32 @@ class SaleOrder(models.Model):
                 order.so_prc_mcv_reel = 0.0
 
     # Méthode create : mise à jour du mode de règlement
-    @api.model
-    def create(self, vals):
-        if 'partner_id' in vals:
-            partner = self.env['res.partner'].browse(vals['partner_id'])
-            vals['x_studio_mode_de_rglement_1'] = partner.x_studio_mode_de_rglement_1
-        return super(SaleOrder, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        fma_tag = self.env['crm.tag'].search([('name', '=', 'FMA')], limit=1)
+        f2m_tag = self.env['crm.tag'].search([('name', '=', 'F2M')], limit=1)
+        for vals in vals_list:
+            if 'partner_id' in vals:
+                partner = self.env['res.partner'].browse(vals['partner_id'])
+                vals['x_studio_mode_de_rglement_1'] = partner.x_studio_mode_de_rglement_1
+
+            if 'tag_ids' in vals:
+                tag_updates = vals.get('tag_ids', [])
+                if tag_updates and fma_tag.id in tag_updates[0][2]:
+                    warehouse_regripiere = self.env['stock.warehouse'].search([('name', '=', 'LA REGRIPPIERE')], limit=1)
+                    if warehouse_regripiere:
+                        vals['warehouse_id'] = warehouse_regripiere.id
+                if tag_updates and f2m_tag.id in tag_updates[0][2]:
+                    warehouse_remaudiere = self.env['stock.warehouse'].search([('name', '=', 'LA REMAUDIERE')], limit=1)
+                    if warehouse_remaudiere:
+                        vals['warehouse_id'] = warehouse_remaudiere.id
+
+        return super(SaleOrder, self).create(vals_list)
 
     # Méthode write fusionnée
     def write(self, vals):
         _logger.info("Appel de write avec vals: %s", vals)
-        
+
         # Si partner_id est modifié, mettre à jour le champ x_studio_mode_de_rglement_1
         if 'partner_id' in vals:
             partner = self.env['res.partner'].browse(vals['partner_id'])
@@ -164,12 +179,12 @@ class SaleOrder(models.Model):
         # Appel de la méthode write parente
         res = super(SaleOrder, self).write(vals)
         _logger.info("Devis mis à jour: %s", self.id)
-        
+
         # Mise à jour de l'entrepôt si les tags sont modifiés
         if 'tag_ids' in vals:
             _logger.info("Appel de _update_warehouse après mise à jour")
             self._update_warehouse()
-        
+
         return res
 
     # Mise à jour de l'entrepôt en fonction des tags
