416ea96f0bf14ff8f6b69b14292434182a044f3f
ALEBOR <a.bordier@joinerybs.net>
2024-09-24 12:37:37 +0200

    bl

--- custom_delivery/models/stock_move.py
+++ custom_delivery/models/stock_move.py
@@ -1,14 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import re
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
     cleaned_description = fields.Char(string="Description Sans Crochets", compute='_compute_cleaned_description')
 
+    @api.depends('name')
     def _compute_cleaned_description(self):
         for move in self:
-            # Enlever le texte entre crochets
             if move.name:
-                move.cleaned_description = re.sub(r'\[.*?\]', '', move.name)
+                # Enlever le texte entre crochets dans la description
+                move.cleaned_description = re.sub(r'\[.*?\]', '', move.name).strip()
             else:
                 move.cleaned_description = move.name
