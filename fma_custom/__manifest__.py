c4425d0deeb745483feacfe34f49ac97ddf7e4a7
Pratima Gupta <pgu@odoo.com>
2024-09-25 10:22:30 +0530

    [IMP] added module to hide the View invoice button from email

---
+++ fma_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "FMA: Custom",
+    "description": """
+        Custom module related changes.
+    """,
+    "summary": "Custom related changes.",
+    "author": "Odoo PS",
+    "version": "15.0.0.1.0",
+    "depends": [
+        "custom"
+    ],
+    "data": [
+        "views/mail_templates.xml",
+    ],
+    "license": "LGPL-3",
+}
