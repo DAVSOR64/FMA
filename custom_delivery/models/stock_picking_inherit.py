88fc42b376c8f6638f98874bbd9674425c2b3cfc
ALEBOR <a.bordier@joinerybs.net>
2024-09-26 09:14:08 +0200

    Bl

--- custom_delivery/models/stock_picking_inherit.py
+++ custom_delivery/models/stock_picking_inherit.py
@@ -4,7 +4,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     # Champs liés au sale.order
-    so_acces = fields.Char(related='sale_order_id.so_acces', string="Accès")
-    so_type_camion = fields.Char(related='sale_order_id.so_type_camion', string="Type de camion")
-    so_horaire_ouverture = fields.Char(related='sale_order_id.so_horaire_ouverture', string="Horaire d'ouverture")
-    so_horaire_fermeture = fields.Char(related='sale_order_id.so_horaire_fermeture', string="Horaire de fermeture")
+    so_acces_bl = fields.Char(related='sale_id.so_acces', string="Accès")
+    so_type_camion_bl = fields.Char(related='sale_id.so_type_camion', string="Type de camion")
+    so_horaire_ouverture_bl = fields.Char(related='sale_id.so_horaire_ouverture', string="Horaire d'ouverture")
+    so_horaire_fermeture_bl = fields.Char(related='sale_id.so_horaire_fermeture', string="Horaire de fermeture")
