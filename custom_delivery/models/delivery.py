7b8d11cba40b157d08ed16dc3dff7157d27da317
DAVSOR64 <newdsa64@gmail.com>
2024-10-08 05:27:40 +0000

    SQLITE

--- custom_delivery/models/delivery.py
+++ custom_delivery/models/delivery.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api, re
 
 class SaleOrder(models.Model):
-    _inherit = 'delivery'
+    _inherit = ['delivery', 'sale.order']
 
     def format_amount(self, amount):
         return '{:,.2f}'.format(amount).replace(',', ' ').replace('.', ',')
