533986a4408048e39573b7258ef061f84e796809
DAVSOR64 <newdsa64@gmail.com>
2024-10-22 12:05:14 +0000

    Saleorder

--- custom/models/sale_order.py
+++ custom/models/sale_order.py
@@ -73,9 +73,7 @@ class SaleOrder(models.Model):
     so_date_de_commande = fields.Date(string="Date de la commande")
     so_date_bpe = fields.Date(string="BPE du : ")
     so_date_de_reception_devis = fields.Date(
-    string="Demande reçue le :",
-    required=True 
-)
+    string="Demande reçue le :")
     so_date_du_devis = fields.Date(string="Devis fait le : ")
     so_date_de_modification_devis = fields.Date(string="Devis modifié le : ")
     so_date_devis_valide = fields.Date(string="Devis validé le : ")
