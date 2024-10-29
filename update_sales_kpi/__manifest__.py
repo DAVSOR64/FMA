e54ef8f274742b49936ebdbc7acb801e93be060a
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2024-10-29 10:37:44 +0100

    Update __manifest__.py

--- update_sales_kpi/__manifest__.py
+++ update_sales_kpi/__manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '17.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale','mrp', 'custom' ],
