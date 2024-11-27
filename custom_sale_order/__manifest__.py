bd6bcbf34bb06a313915916d79341486be7a294a
supr-odoo <supr@odoo.com>
2024-11-27 15:47:42 +0530

    [custom_sale_order] changes in report template + updated manifest

--- custom_sale_order/__manifest__.py
+++ custom_sale_order/__manifest__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': "custom_sale_order",
 
@@ -9,9 +10,9 @@
         This modul customize the sale_order odf
     """,
 
-    'author': "My Company",
+    'author': "Odoo PS",
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '17.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['sale'],
