1d75e1b0df053b2925922c072a847b1e507e8c5a
supr-odoo <supr@odoo.com>
2024-11-27 17:18:18 +0530

    [custom_invoice] updated report_invoce.xml + manifest

--- custom_invoice/__manifest__.py
+++ custom_invoice/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Custom Invoice Text Block',
-    'version': '1.0',
+    'version': '17.0.1.0.1',
     'summary': 'Show text block on invoice based on contact boolean field',
-    'author': 'Your Name',
-    'depends': ['account'],
+    'author': 'Odoo PS',
+    'depends': ['account', 'custom'],
     'data': [
          'views/report_invoice.xml',
     ],
