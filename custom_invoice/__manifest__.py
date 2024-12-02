0105c84cdfcac9b82b1dc05e9fb4bba190e32642
supr-odoo <supr@odoo.com>
2024-12-02 09:59:39 +0530

    [custom_invoice] remove extra lines from py files + updated views

--- custom_invoice/__manifest__.py
+++ custom_invoice/__manifest__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Custom Invoice Text Block',
     'version': '17.0.1.0.1',
     'summary': 'Show text block on invoice based on contact boolean field',
-    'author': 'Odoo PS',
+    'author': 'Your Name',
     'depends': ['account', 'custom'],
     'data': [
          'views/report_invoice.xml',
