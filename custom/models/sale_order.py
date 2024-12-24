dd9aca548777965d970b914bf8c234c3bc4ab717
supr-odoo <supr@odoo.com>
2024-12-24 16:30:24 +0530

    [IMP] Added required in views

--- custom/models/sale_order.py
+++ custom/models/sale_order.py
@@ -78,7 +78,7 @@ class SaleOrder(models.Model):
     so_date_de_modification = fields.Date(string="Date de modification")
     so_date_de_commande = fields.Date(string="Date de la commande")
     so_date_bpe = fields.Date(string="BPE du : ")
-    so_date_de_reception_devis = fields.Date(string="Demande reçue le :", required=True)
+    so_date_de_reception_devis = fields.Date(string="Demande reçue le :")
     so_date_du_devis = fields.Date(string="Devis fait le : ")
     so_date_de_modification_devis = fields.Date(string="Devis modifié le : ")
     so_date_devis_valide = fields.Date(string="Devis validé le : ")
@@ -105,7 +105,6 @@ class SaleOrder(models.Model):
             ('WICONA','WICONA'),
         ],
         string="GAMME",
-        required=True,
     )
 
     so_serie = fields.Selection(
@@ -233,7 +232,6 @@ class SaleOrder(models.Model):
             ('WT50.RC3', 'WT50.RC3'),
         ],
         string="SERIE",
-        required=True,
     )
 
 
