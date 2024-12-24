133beee0067aacb63579a631bd54260b0f6956ab
Pratima Gupta <pgu@odoo.com>
2024-12-24 15:39:56 +0530

    [ADD] improvements for making the commit green

--- custom_delivery/__manifest__.py
+++ custom_delivery/__manifest__.py
@@ -14,7 +14,7 @@
     'website': "http://www.yourcompany.com",
 
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'stock'],
