40a9219bc522d9e5c1dddd2091c23eb25bdf2da6
Pratima Gupta <pgu@odoo.com>
2025-01-08 10:06:02 +0530

    [IMP] added code to export the file on vendor specific folders + module is upgraded in 17 for SFTP

--- purchase_order_export/models/__init__.py
+++ purchase_order_export/models/__init__.py
@@ -2,6 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import ir_attachment
-from . import purchase_order
-from . import res_config_settings
 from . import res_partner
+from . import res_config_settings
+from . import purchase_order
