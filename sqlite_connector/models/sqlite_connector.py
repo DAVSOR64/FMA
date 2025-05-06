5b60f5e87c7e87b7609e6c3abf919201e02b1c1c
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-05-06 10:37:54 +0200

    Update sqlite_connector.py

--- sqlite_connector/models/sqlite_connector.py
+++ sqlite_connector/models/sqlite_connector.py
@@ -735,17 +735,18 @@ class SqliteConnector(models.Model):
        
         # Process data for profile
         #BP = ''
-        resultBP=cursor.execute("select subNode, FieldName, SValue from REPORTVARIABLES")
-        for row in resultBP :
-            if (row[0] == 'UserVars') and (row[1] == 'UserInteger1') :
-                if (row[2] == '1')  :
-                    BP = 'BPA'
-                if (row[2] == '3') :
-                    BP = 'BPE'
-                if (row[2] == '2') :
-                    BP = 'BPA-BPE'
+        #resultBP=cursor.execute("select subNode, FieldName, SValue from REPORTVARIABLES")
+        #for row in resultBP :
+        #    if (row[0] == 'UserVars') and (row[1] == 'UserInteger1') :
+        #        if (row[2] == '1')  :
+        #            BP = 'BPA'
+        #        if (row[2] == '3') :
+        #            BP = 'BPE'
+        #        if (row[2] == '2') :
+        #            BP = 'BPA-BPE'
                 
         _logger.warning("Profile %s " % BP )
+        BP = 'BPE'
         if BP == 'BPA' or BP == 'BPE':
             resultpf = cursor.execute("select AllProfiles.ArticleCode, AllProfiles.Description, AllProfiles.ArticleCode_Supplier, AllProfiles.Description, AllProfiles.Color, AllProfiles.Price, AllProfiles.Units, AllProfiles.Amount, AllProfiles.IsManual, AllProfiles.OuterColorInfoInternal, AllProfiles.InnerColorInfoInternal, AllProfiles.ColorInfoInternal, AllProfiles.ArticleCode_BaseNumber, AllProfiles.ArticleCode_Number, AllProfiles.PUSize, AllProfiles.Length  from AllProfiles order by AllProfiles.ArticleCode_Supplier")
             idun =''
