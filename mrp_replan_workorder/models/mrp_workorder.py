aad83fd7e17718944010f04d9c2911b7f2599e35
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-05-28 09:34:26 +0200

    MRP

--- mrp_replan_workorder/models/mrp_workorder.py
+++ mrp_replan_workorder/models/mrp_workorder.py
@@ -1,77 +1,22 @@
-from odoo import models, fields
+from odoo import models
+
 
 class MrpWorkorder(models.Model):
-    _inherit = 'mrp.workorder'
+    _inherit = "mrp.workorder"
 
     def write(self, values):
-        context = self.env.context
-
-        # Calculer le delta AVANT d'écrire
-        deltas = {}
-        for wo in self:
-            old_date = wo.date_start
-            new_date_raw = values.get('date_start', old_date)
-            new_date = fields.Datetime.from_string(new_date_raw) if isinstance(new_date_raw, str) else new_date_raw
-            if old_date and new_date and old_date != new_date:
-                delta = new_date - old_date
-                deltas[wo.id] = delta
-
-        # Appeler l'écriture standard
         res = super().write(values)
-
-        # Après l'écriture, appliquer la logique
-        for wo in self:
-            delta = deltas.get(wo.id)
-            if delta:
-                if wo.is_first_workorder() or context.get('from_gantt'):
-                    wo._shift_entire_of_and_dependents(delta)
-                else:
-                    wo._shift_partial_of(delta)
+        trigger_fields = {'date_start', 'date_finished'}
+        if trigger_fields.intersection(values.keys()):
+            self._replan_workorder()
         return res
 
-
-    def is_first_workorder(self):
-        self.ensure_one()
-        return self == self.production_id.workorder_ids.sorted(lambda wo: wo.operation_id.sequence or 0)[0]
-
-
-    def _shift_entire_of_and_dependents(self, delta):
-        """Décale tout l'OF + les OFs suivants."""
-        # Décaler tous les WOs de l'OF courant
-        for wo in self.production_id.workorder_ids:
-            if wo.date_start:
-                wo.date_start += delta
-            if wo.date_finished:
-                wo.date_finished += delta
-
-        # Décaler les OFs suivants (via les dépendances)
+    def _replan_workorder(self):
         for wo in self:
-            for next_wo in wo.needed_by_workorder_ids:
-                next_wo.date_start += delta
-                next_wo.date_finished += delta
-                next_wo._shift_entire_of_and_dependents(delta)
-
-    def _shift_partial_of(self, delta):
-        """Décale ce WO et les suivants dans l’OF."""
-        self.ensure_one()
-        wos_to_shift = self.production_id.workorder_ids.filtered(
-            lambda w: (w.operation_id.sequence or 0) >= (self.operation_id.sequence or 0)
-        )
-        for wo in wos_to_shift:
-            if wo.date_start:
-                wo.date_start += delta
-            if wo.date_finished:
-                wo.date_finished += delta
-
-
-    def manual_swap(self, other_wo):
-        """Permuter cet ordre de travail avec un autre sur le même poste."""
-        if self.workcenter_id != other_wo.workcenter_id:
-            raise ValueError("Les ordres de travail ne sont pas sur le même poste.")
-        # Inverser les séquences (ou dates) des WOs
-        self_sequence = self.sequence
-        other_sequence = other_wo.sequence
-        self.with_context(manual_swap=True).write({'sequence': other_sequence})
-        other_wo.with_context(manual_swap=True).write({'sequence': self_sequence})
-
-
+            conflicted_dict = wo._get_conflicted_workorder_ids()
+            if conflicted_dict.get(wo.id):
+                for conflicted in self.browse(conflicted_dict.get(wo.id)):
+                    conflicted._plan_workorder(replan=True)
+            for needed in wo.needed_by_workorder_ids:
+                needed._plan_workorder(replan=True)
+                needed._replan_workorder()
