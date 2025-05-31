88fe8aec38d6d7e2c962791d94996121d4af1ab2
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-05-31 09:55:47 +0200

    Update sale_advance_payment_inv.py

--- custom/models/sale_advance_payment_inv.py
+++ custom/models/sale_advance_payment_inv.py
@@ -4,7 +4,6 @@ from odoo.exceptions import UserError
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = 'sale.advance.payment.inv'
 
-    @api.multi
     def create_invoices(self):
         for wizard in self:
             sales = self.env['sale.order'].browse(self._context.get('active_ids', []))
@@ -14,7 +13,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
                 # Vérifie si le client a l'étiquette "Entreprise Générale"
                 if partner.category_id.filtered(lambda c: c.name == "Entreprise Générale"):
                     # Remplace 'x_studio_champ' par le nom technique exact de ton champ Studio à vérifier
-                    if not partner.x_studio_champ:
+                    if not sale.so_commande_client:
                         raise UserError(
                             f"Le champ requis n'est pas rempli pour le client {partner.name}. Impossible de créer la facture."
                         )
