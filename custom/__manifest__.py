fbc19091cb50cf9a418bafa5918f3d08bd76fd74
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-05-31 09:47:38 +0200

    Update __manifest__.py

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '17.0.1.0.1',
     'summary': 'Created and Transfer custom field from contact and sale order to invoice',
     'author': 'Your Name',
-    'depends': ['base','sale', 'account','contacts', 'sale_stock','mrp'],
+    'depends': ['base','sale', 'account','contacts', 'sale_stock','mrp','sale_management'],
     'data': [
         'views/res_partner_views.xml',
         'views/sale_order_views.xml',
