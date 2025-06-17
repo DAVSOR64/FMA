f70bda79307ed8e14baadc917ef30d9a3fd50a67
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-06-17 18:51:16 +0200

    Update sqlite_connector.py

--- sqlite_connector/models/sqlite_connector.py
+++ sqlite_connector/models/sqlite_connector.py
@@ -1473,7 +1473,7 @@ class SqliteConnector(models.Model):
         # Étape 1: Lire la table SQL et agréger les données
         aggregated_data = {}
 
-        cursor.execute("SELECT COUNT(*) FROM CalcDetailItem")
+        cursor.execute("SELECT COUNT(*) FROM CalcDetailItems")
         count = cursor.fetchone()[0]
 
         _logger.warning("Nombre de lignes %s " % str(count) )
