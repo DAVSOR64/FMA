4104c87bde3da8c14a8d5c4397bfe09122c72fa0
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-06-19 16:15:12 +0200

    Update mrp_production.py

--- mrp_replan_workorder/models/mrp_production.py
+++ mrp_replan_workorder/models/mrp_production.py
@@ -13,12 +13,12 @@ class MrpProduction(models.Model):
                 if not previous_op:
                     workorder.date_planned_start = production.date_planned_start
                 else:
-                    delay_obj = self.env['x_delai_entre_operatio_line_10114'].search([
+                    delay_obj = self.env['x_delai_entre_operatio'].search([
                         ('x_studio_poste_de_travail_deb', '=', previous_op.workcenter_id.id),
                         ('x_studio_poste_de_travail_fin', '=', workorder.workcenter_id.id)
                     ], limit=1)
 
-                    delay_minutes = delay_obj.x_studio_delai if delay_obj else 0
+                    delay_minutes = delay_obj.x_studio_dlai_entre_opration if delay_obj else 0
                     start_date = previous_op.date_planned_finished + timedelta(minutes=delay_minutes)
                     workorder.date_planned_start = start_date
 
