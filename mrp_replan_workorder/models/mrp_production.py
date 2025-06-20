b7ec53868ba813b10ef414050d299ec82f0e9290
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-06-20 13:37:40 +0200

    Update mrp_production.py

--- mrp_replan_workorder/models/mrp_production.py
+++ mrp_replan_workorder/models/mrp_production.py
@@ -15,7 +15,7 @@ class MrpProduction(models.Model):
 
         for production in self:
             previous_op = None
-            for workorder in production.workorder_ids.sorted('sequence'):
+            for workorder in sorted(production.workorder_ids, key=lambda wo: wo.operation_id.sequence):
                 if not previous_op:
                     workorder.date_planned_start = production.date_planned_start
                 else:
@@ -25,13 +25,13 @@ class MrpProduction(models.Model):
                     ], limit=1)
                     
                     delay_minutes = delay_obj.x_studio_dlai_entre_opration if delay_obj else 0
-                    _logger.warning("**********Delai********* %s " % str(delay_minutes) )
+                    _logger.warning("**********Delai********* %s " % str(delay_minutes))
                     start_date = previous_op.date_planned_finished + timedelta(minutes=delay_minutes)
                     workorder.date_planned_start = start_date
-
+        
                 duration = workorder.duration_expected or 0.0
                 workorder.date_planned_finished = workorder.date_planned_start + timedelta(minutes=duration)
-
+        
                 previous_op = workorder
 
         return res
