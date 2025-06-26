5a53b4f70cdc0acc80f955e98cf3985d83032abd
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-06-26 08:52:27 +0200

    Update export_config.py

--- export_powerbi/models/export_config.py
+++ export_powerbi/models/export_config.py
@@ -1,11 +1,10 @@
-from odoo import models, fields
+from odoo import fields, models
 
-class ExportSFTPConfig(models.Model):
-    _name = 'export.sftp.config'
-    _description = 'Configuration SFTP pour export Power BI'
 
-    host = fields.Char('Hôte SFTP', required=True)
-    port = fields.Integer('Port', default=22)
-    username = fields.Char('Utilisateur', required=True)
-    password = fields.Char('Mot de passe', required=True)
-    path = fields.Char('Répertoire distant', default='/')
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    sftp_server_host = fields.Char(config_parameter='fma_powerbi_export.sftp_server_host')
+    sftp_server_username = fields.Char(config_parameter='fma_powerbi_export.sftp_server_username')
+    sftp_server_password = fields.Char(config_parameter='fma_powerbi_export.sftp_server_password')
+    sftp_server_file_path = fields.Char(config_parameter='fma_powerbi_export.sftp_server_file_path')
