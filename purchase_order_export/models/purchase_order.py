68468db5aa201497bc186b2149e8b7987455b991
ALEBOR44 <a.bordier@joinerybs.net>
2025-07-02 16:15:57 +0200

    Update purchase_order.py

--- purchase_order_export/models/purchase_order.py
+++ purchase_order_export/models/purchase_order.py
@@ -135,6 +135,7 @@ class PurchaseOrder(models.Model):
                     'res_model': 'purchase.order',
                     'res_id': po.id,
                     'mimetype': mimetype,
+                    'is_po_xml': True,
                 })
 
             except Exception as e:
