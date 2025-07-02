46647e54c52aa6c33437b85698ca6af9e168c884
ALEBOR44 <a.bordier@joinerybs.net>
2025-07-02 08:59:11 +0200

    Create po_export_wizard.py

---
+++ purchase_order_export/wizard/po_export_wizard.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields
+
+class PoExportWizard(models.TransientModel):
+    _name = 'po.export.wizard'
+    _description = 'Export Order Wizard'
+
+    export_format = fields.Selection([
+        ('xlsx', 'Excel (REYNAERS)'),
+        ('xml', 'XML (ComAlu)'),
+        ('xml_v2', 'XML (TIV)')
+    ], string='Export Format', default='xlsx')
+
+    def action_export(self):
+        active_ids = self.env.context.get('active_ids', [])
+        purchase_orders = self.env['purchase.order'].browse(active_ids)
+        for po in purchase_orders:
+            po.action_export_order(self.export_format)
