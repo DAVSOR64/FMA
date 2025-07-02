a6de2b3dfb1f037d937783ef16babecbf4c3735c
ALEBOR44 <a.bordier@joinerybs.net>
2025-07-02 08:43:52 +0200

    Create product.py

---
+++ purchase_order_export/models/product.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    clc1 = fields.Char(string="Clc1")
+    clc2 = fields.Char(string="Clc2")
+    cls1 = fields.Char(string="Cls1")
+    cls2 = fields.Char(String="Cls2")
