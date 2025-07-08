0a87d1a57f9d8f76a4fdc3d60fba9fc72cfb55f8
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-07-08 14:31:23 +0200

    Update sqlite_connector.py

--- sqlite_connector/models/sqlite_connector.py
+++ sqlite_connector/models/sqlite_connector.py
@@ -1387,7 +1387,8 @@ class SqliteConnector(models.Model):
             pro = self.env['product.product'].search([('default_code', '=', pro_name)], limit=1)
             if sale_order:
             # stagging before merge if sale_order and so_data:
-               if pro and so_data[sale_order.id] and so_data[sale_order.id].get('order_line'):
+               #if pro and so_data[sale_order.id] and so_data[sale_order.id].get('order_line'):
+                if pro and sale_order.id in so_data and so_data[sale_order.id].get('order_line'):
                     so_data[sale_order.id].get('order_line').append(Command.create({
                         'product_id': pro.id,
                         'price_unit': 0,
