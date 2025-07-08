c0b31811f32c32dff982cfbacf203001d9afc37c
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-07-08 15:29:10 +0200

    Update sqlite_connector.py

--- sqlite_connector/models/sqlite_connector.py
+++ sqlite_connector/models/sqlite_connector.py
@@ -1518,13 +1518,13 @@ class SqliteConnector(models.Model):
         # Étape 1: Lire la table SQL et agréger les données
         aggregated_data = {}
 
-        resu = cursor.execute("SELECT * FROM LabourTimes")
-        for row in resu :
+        #resu = cursor.execute("SELECT * FROM LabourTimes")
+        #for row in resu :
             #_logger.warning("Temps %s " % str(row[7]))        
         
-        cursor.execute("SELECT COUNT(*) FROM LabourTimes")
-        count = cursor.fetchone()[0]    
-        _logger.warning("Nombre de lignes %s " % str(count) )
+        #cursor.execute("SELECT COUNT(*) FROM LabourTimes")
+        #count = cursor.fetchone()[0]    
+        #_logger.warning("Nombre de lignes %s " % str(count) )
         
         resuOpe = []
         cursor.execute("""
