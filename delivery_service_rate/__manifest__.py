ef6a1342dd67b00cafe3ef6b15a33defffb93c97
ALEBOR44 <a.bordier@joinerybs.net>
2025-07-22 14:57:29 +0200

    Update __manifest__.py

--- delivery_service_rate/__manifest__.py
+++ delivery_service_rate/__manifest__.py
@@ -9,7 +9,6 @@
         'views/delivery_service_rate_view.xml',
         'data/delivery_service_rate_view.sql',
     ],
-    'pre_init_hook': 'pre_init_hook',
     'installable': True,
     'application': False,
 }
