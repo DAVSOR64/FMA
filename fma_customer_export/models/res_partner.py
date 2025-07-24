c508f9be43ca8ab59a4b3ffe6b46862d5bb59ce9
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-07-24 14:35:57 +0200

    Update res_partner.py

--- fma_customer_export/models/res_partner.py
+++ fma_customer_export/models/res_partner.py
@@ -51,7 +51,7 @@ class ResPartner(models.Model):
                     str(partner.siret or '').ljust(14),
                     '      ',
                     str(partner.vat or '').ljust(14),
-                    str(partner.x_studio_commercial_1.name or '').ljust(50),
+                    '                                                  ',
                     '             ',
                     '                         ',
                     'EUR',
