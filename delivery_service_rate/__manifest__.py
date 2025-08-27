abbb41227d09ed30e86f932c81280d6308e7cf83
ALEBOR44 <a.bordier@joinerybs.net>
2025-08-27 14:36:50 +0200

    Update __manifest__.py

--- delivery_service_rate/__manifest__.py
+++ delivery_service_rate/__manifest__.py
@@ -2,13 +2,14 @@
     'name': 'Delivery Service Rate',
     'version': '1.0',
     'depends': ['stock'],
-    'author': 'AleBor',
+    'author': 'TonNom',
     'category': 'Warehouse',
     'summary': 'Calcul du taux de service (livraison à temps)',
     'data': [
         'views/delivery_service_rate_view.xml',
         'data/delivery_service_rate_view.sql',
     ],
+    'pre_init_hook': 'pre_init_hook',
     'installable': True,
     'application': False,
 }
