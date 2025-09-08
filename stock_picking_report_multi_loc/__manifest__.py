f8e2286353e4dd248a957691dff3b9891c03a8b4
ALEBOR44 <a.bordier@joinerybs.net>
2025-09-08 10:27:00 +0200

    Create __manifest__.py

---
+++ stock_picking_report_multi_loc/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Picking Report – Multi Locations (v17)",
+    "summary": "Bon de préparation: retire la colonne destination, ajoute 'Emplacements existants', affiche tous les articles.",
+    "version": "17.0.1.0.1",
+    "author": "Vous",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": ["views/report_picking_inherit.xml"],
+    "installable": True,
+    "application": False,
+}
