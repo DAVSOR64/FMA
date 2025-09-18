c349754ca53cca764aa19c2e065d397ada2a71a2
ALEBOR44 <a.bordier@joinerybs.net>
2025-09-18 14:40:48 +0200

    Update __manifest__.py

--- export_powerbi/__manifest__.py
+++ export_powerbi/__manifest__.py
@@ -1,8 +1,8 @@
 {
     'name': 'Export PowerBI',
-    'version': '17.0.1.0',
+    'version': '17.0.1.1',
     'summary': 'Export clients, commandes, factures vers SFTP pour Power BI',
-    'depends': ['base', 'sale', 'account'],
+    'depends': ['base', 'sale', 'account', 'custom'], 
     'data': [
         'data/ir_cron.xml',
         'views/export_config_views.xml',
