272deb5792ab184c954c0d9968e89c6b67cae417
ALEBOR44 <a.bordier@joinerybs.net>
2025-09-24 09:08:28 +0200

    Create __manifest__.py

---
+++ sale_kpi_delivery/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "KPI Facturation",
+    "version": "17.0.1.0.0",
+    "category": "Sales/Reporting",
+    "summary": "Facturé & RAF par affaire, mois/semaine de livraison planifiée, filtrable par tags",
+    "depends": ["sale_management", "stock", "account"],
+    "data": ["views/kpi_report_views.xml"],
+    "license": "LGPL-3",
+    "installable": True,
+}
