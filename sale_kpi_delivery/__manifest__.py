a83fba7c5b1d88480a122260abe939ac313268d8
ALEBOR44 <a.bordier@joinerybs.net>
2025-09-24 11:48:32 +0200

    Update __manifest__.py

--- sale_kpi_delivery/__manifest__.py
+++ sale_kpi_delivery/__manifest__.py
@@ -1,10 +1,13 @@
 {
     "name": "KPI Facturation",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Sales/Reporting",
     "summary": "Facturé & RAF par affaire, mois/semaine de livraison planifiée, filtrable par tags",
     "depends": ["sale_management", "stock", "account"],
-    "data": ["views/kpi_report_views.xml"],
+    "data": [
+        "views/kpi_report_views.xml",
+        "views/sale_orders_to_invoice_or_no.xml",
+    ],
     "license": "LGPL-3",
     "installable": True,
 }
