ab29e316f8c7e3bd12ee341c74f6620c908793f1
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2025-10-23 16:48:03 +0200

    Update res_partner.py

--- fma_supplier_export/models/res_partner.py
+++ fma_supplier_export/models/res_partner.py
@@ -128,7 +128,7 @@ class ResPartner(models.Model):
             # Mark the record as included
             partner.is_included_in_customers_export_file = True
 
-    def cron_generate_generate_customer_files(self):
+    def cron_generate_generate_supplier_files(self):
         """Cron to generate supplier details .txt file."""
         partners = self.search([
             ('is_included_in_supplier_export_file', '=', False),
