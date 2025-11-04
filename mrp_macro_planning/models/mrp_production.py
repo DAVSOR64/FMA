6ba6c43f89ec2f0220ff430302c5465f5ec9643a
ayou-infolib <aachour@infolib.tn>
2025-11-04 10:07:36 +0100

    [17][FIX] Date Macro Planning

--- mrp_macro_planning/models/mrp_production.py
+++ mrp_macro_planning/models/mrp_production.py
@@ -1,14 +1,12 @@
 import pytz
-
 from datetime import datetime, time
-
 from odoo import api, models, fields
 
 
 class MrpWorkOrder(models.Model):
     _inherit = "mrp.workorder"
 
-    date_macro = fields.Date("Macro Date")
+    date_macro = fields.Datetime("Macro Date")
 
 
 class MrpProduction(models.Model):
@@ -25,10 +23,16 @@ class MrpProduction(models.Model):
 
     def _compute_date_macro(self):
         for production in self:
-            date_delivery = production.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id.commitment_date or production.date_finished
+            date_delivery = (
+                production.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id.commitment_date
+                or production.date_finished
+            )
+
             manufacturing_lead = production.company_id.manufacturing_lead
-            calendar = production.company_id.resource_calendar_id or self.env.ref('resource.resource_calendar_std')
-            # date_delivery = date_delivery.replace(hour=8, minute=0, second=0, microsecond=0)
+            calendar = (
+                production.company_id.resource_calendar_id
+                or self.env.ref('resource.resource_calendar_std')
+            )
 
             # Calcul de la deadline en jours ouvrés
             deadline_production = calendar.plan_days(-manufacturing_lead, date_delivery)
@@ -41,10 +45,25 @@ class MrpProduction(models.Model):
                 last_date2 = workcenter_calendar._attendance_intervals_batch(
                     datetime.combine(last_date, time.min).replace(tzinfo=pytz.UTC),
                     datetime.combine(last_date, time.max).replace(tzinfo=pytz.UTC),
-                    resources=work.workcenter_id.resource_id)
-                last_date2 = last_date2[work.workcenter_id.resource_id.id]._items[0][0]
-                last_date = last_date.replace(hour=last_date2.hour, minute=0, second=0, microsecond=0)
+                    resources=work.workcenter_id.resource_id
+                )
+
+                if last_date2 and work.workcenter_id.resource_id.id in last_date2:
+                    first_interval = last_date2[work.workcenter_id.resource_id.id]._items
+                    if first_interval:
+                        last_date2 = first_interval[0][0]
+                        last_date = last_date.replace(
+                            hour=last_date2.hour, minute=0, second=0, microsecond=0
+                        )
 
                 last_date = workcenter_calendar.plan_hours(-work.duration_expected / 60, last_date)
                 last_date = workcenter_calendar.plan_days(-1, last_date)
 
+            # 🔹 Mise à jour des dates de production
+            if production.workorder_ids:
+                all_dates = [w.date_macro for w in production.workorder_ids if w.date_macro]
+                if all_dates:
+                    production.sudo().with_context(no_recompute=True).update({
+                        'date_start': min(all_dates),
+                        'date_finished': max(all_dates),
+                    })
