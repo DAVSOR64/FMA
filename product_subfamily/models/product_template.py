3b8c25d0d865a05474c0fcc41b22f2a664315418
ALEBOR44 <a.bordier@joinerybs.net>
2025-11-19 15:39:03 +0100

    Create product_template.py

---
+++ product_subfamily/models/product_template.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    subfamily_id = fields.Many2one(
+        'product.subfamily',
+        string="Sous-famille",
+        domain="[('product_categ_id', '=', categ_id)]",
+        help="Sous-famille dépendante de la catégorie de produit.",
+    )
