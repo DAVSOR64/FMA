2c597436296ff4519296395d67327d44690f0b94
ALEBOR44 <a.bordier@joinerybs.net>
2025-11-19 15:52:20 +0100

    Create __manifest__.py

---
+++ product_subfamily/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Product Subfamily",
+    "summary": "Gestion de sous-familles de produits liées aux catégories",
+    "version": "16.0.1.0.0",
+    "author": "ALEBOR",
+    "license": "LGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_subfamily_views.xml",
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
