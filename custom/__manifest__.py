ad17366cc0ada979c704ae5749f946dd5a03600e
ALEBOR44 <a.bordier@joinerybs.net>
2025-11-26 15:12:22 +0100

    Update __manifest__.py

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -4,11 +4,16 @@
     'version': '17.0.1.0.1',
     'summary': 'Created and Transfer custom field from contact and sale order to invoice',
     'author': 'Your Name',
-    'depends': ['base','sale', 'account','contacts', 'sale_stock','mrp','sale_management'],
+    'depends': ['base','sale', 'account','contacts', 'sale_stock','mrp'],
     'data': [
+        'security/ir.model.access.csv',
         'views/res_partner_views.xml',
         'views/sale_order_views.xml',
         'views/account_move_views.xml',
+        'views/affair_chat_template_views.xml',
+        'data/message_templates.xml',
+        'data/mail_template_retard.xml',
+        
     ],
     'assets': {
         'web.assets_backend': [
