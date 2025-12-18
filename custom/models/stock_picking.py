631548f46c2b4751bde151274a142c8c60246e91
ALEBOR44 <a.bordier@joinerybs.net>
2025-12-18 17:16:10 +0100

    Update stock_picking.py

--- custom/models/stock_picking.py
+++ custom/models/stock_picking.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
 
@@ -6,19 +5,17 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     so_retard_motif_level1_id = fields.Many2one(
-        "sale.delay.reason",
-        string="Motif retard - Niveau 1",
-        domain="[('level','=','1'), ('active','=',True)]",
+        "sale.delay.category",
+        string="Motif",
     )
 
     so_retard_motif_level2_id = fields.Many2one(
         "sale.delay.reason",
-        string="Motif retard - Niveau 2",
-        domain="[('level','=','2'), ('active','=',True), ('parent_id','=',so_retard_motif_level1_id)]",
+        string="Désignation",
+        domain="[('category_id', '=', so_retard_motif_level1_id)]",
     )
 
     @api.onchange("so_retard_motif_level1_id")
     def _onchange_so_retard_motif_level1_id(self):
-        """Quand on change le N1, on vide le N2 pour éviter incohérence."""
-        for rec in self:
-            rec.so_retard_motif_level2_id = False
+        # Si on change le Motif (catégorie), on reset la Désignation
+        self.so_retard_motif_level2_id = False
