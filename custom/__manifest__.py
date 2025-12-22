07f13c7430fae03c5755c2be611d909db4d3739c
ALEBOR44 <a.bordier@joinerybs.net>
2025-12-22 15:14:17 +0100

    Update __manifest__.py

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -6,16 +6,16 @@
     "author": "Your Name",
     "depends": ["base", "sale", "account", "contacts", "sale_stock", "mrp"],
     "data": [
-        #"security/ir.model.access.csv",
+        "security/ir.model.access.csv",
         "views/res_partner_views.xml",
         "views/sale_order_views.xml",
         "views/account_move_views.xml",
         "views/affair_chat_template_views.xml",
-        #"views/sale_delay_reason_views.xml",
-        #"views/stock_picking_views.xml",
-        #"data/message_templates.xml",
-        #"data/mail_template_retard.xml",
-        #"data/mail_template_retard_livraison.xml",
+        "views/sale_delay_reason_views.xml",
+        "views/stock_picking_views.xml",
+        "data/message_templates.xml",
+        "data/mail_template_retard.xml",
+        "data/mail_template_retard_livraison.xml",
     ],
     "assets": {
         "web.assets_backend": [
