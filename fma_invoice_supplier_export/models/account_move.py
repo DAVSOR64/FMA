df6023049d6316c79730aa97be374951fc37dd40
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-01-16 14:51:01 +0100

    Update account_move.py

--- fma_invoice_supplier_export/models/account_move.py
+++ fma_invoice_supplier_export/models/account_move.py
@@ -234,7 +234,7 @@ class AccountMove(models.Model):
                         #'mode_de_regiment': move.inv_mode_de_reglement.replace('L.C.R. A L ACCEPTATION', 'L.C.R. A L ACCEPTATI') if move.inv_mode_de_reglement == 'L.C.R. A L ACCEPTATION' else move.inv_mode_de_reglement,
                         "name_and_customer_name": f"{name_invoice} {move.partner_id.name}",
                         "payment_reference": analytic_code or "",
-                        "section_axe2": analytic_code.replace("-", "")
+                        "section_axe2": analytic_code.replace("-", "")[:10]
                         if analytic_code
                         else "",
                         "section": section,
