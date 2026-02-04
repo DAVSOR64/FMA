09029f0f982d12de1ca674d9c91b4bb63ed45598
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-02-04 20:57:52 +0100

    Create mrp_workorder.py

---
+++ mrp_replan_workorder/models/mrp_workorder.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class MrpWorkorder(models.Model):
+    _inherit = "mrp.workorder"
+
+    macro_planned_start = fields.Datetime(
+        string="Macro (début planifié)",
+        help="Début planifié calculé depuis la date de livraison (planning macro).",
+        copy=False,
+        index=True,
+        tracking=False,
+    )
