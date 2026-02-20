c318159094a4a8abd7d74751fceb92dcfb4a16d3
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-02-20 17:52:50 +0100

    Correct Bug

---
+++ mrp_macro_planning_norecalc/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MRP Macro Planning - Capacité vs Charge',
+    'version': '17.0.5.0.0',
+    'category': 'Manufacturing',
+    'summary': 'Tableau de bord capacité vs charge avec suivi prévu/effectué/cumul',
+    'author': 'Paxo Consulting',
+    'depends': ['mrp', 'planning', 'hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/capacite_charge_views.xml',
+        'views/capacite_operateur_views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ mrp_macro_planning_norecalc/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import capacite_charge
+from . import mrp_workorder
+from . import mrp_production
