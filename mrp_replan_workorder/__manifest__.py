e2b3f78e24a49d1b2bbe43c83cd2ff4aeffcf16e
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-02-22 11:39:12 +0100

    Manifest

--- mrp_replan_workorder/__manifest__.py
+++ mrp_replan_workorder/__manifest__.py
@@ -1,9 +1,30 @@
-
 {
-    "name": "MRP Replan Workorder",
-    "version": "18.0.1.0.0",
-    "depends": ["mrp", "sale"],
-    "author": "Custom",
-    "data": [],
-    "installable": true
+    'name': "Replan WorkOrder",
+    'version': "17.0.0.0.2",
+    "license": "LGPL-3",
+    'category': 'Manufacturing',
+    'summary': 'Planification atelier avec règle 1 opération/jour par OF',
+    'description': """
+        Planification intelligente des ordres de fabrication :
+        - 1 opération par jour par OF
+        - Prise en compte des calendriers (jours fériés, week-ends)
+        - Planification à rebours depuis date de livraison
+        - Déclenchement automatique à la validation de commande
+    """,
+    'author': 'Paxo Consulting',
+    'website': 'https://www.paxoconsulting.com',
+    'depends': [
+        'mrp',
+        'sale_mrp',
+        'resource',
+    ],
+    
+    
+    'data': [
+        'views/sale_order_views.xml',
+        'views/mrp_workorder_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
