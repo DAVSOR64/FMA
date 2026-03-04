fd19613103c000d983736867012c457b4ec243da
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-03-04 06:06:00 +0100

    Manifest

---
+++ mrp_macro_planning/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MRP Macro Planning - Capacité vs Charge',
+    'version': '17.0.6.0.0',
+    'category': 'Manufacturing',
+    'summary': 'Tableau de bord capacité vs charge avec suivi prévu/effectué/cumul',
+    'author': 'Paxo Consulting',
+    'depends': ['mrp', 'planning', 'hr', 'mrp_capacity_planning'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/capacite_charge_views.xml',
+        'views/capacite_operateur_views.xml',
+        'views/mrp_workorder_views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
