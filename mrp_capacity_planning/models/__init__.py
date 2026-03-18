af4fa9eed0022b12befa904630526438ffc112ec
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-03-18 21:50:21 +0100

    Update

--- mrp_capacity_planning/models/__init__.py
+++ mrp_capacity_planning/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import mrp_capacity_resource
 from . import mrp_capacity_week
+from . import planning_role
 from . import leave_recompute
--- mrp_capacity_planning/models/leave_recompute.py
+++ mrp_capacity_planning/models/leave_recompute.py
@@ -1,59 +1,63 @@
-
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 from odoo import api, fields, models
 
 
-class MrpCapacityRecomputeMixin(models.AbstractModel):
-    _name = 'mrp.capacity.recompute.mixin'
-    _description = 'Mixin de recalcul capacité'
+def _trigger_capacity_refresh(env):
+    if 'mrp.capacity.week' not in env:
+        return
 
-    def _trigger_capacity_refresh(self):
-        if 'mrp.capacity.week' not in self.env:
-            return
-        today = fields.Date.today()
-        date_from = today - timedelta(days=today.weekday())
-        date_to = date_from + timedelta(weeks=52)
-        self.env['mrp.capacity.week'].sudo().recompute_range(date_from=date_from, date_to=date_to)
-        if 'mrp.capacite.cache' in self.env:
-            self.env['mrp.capacite.cache'].sudo().refresh_all(date_from=date_from, date_to=date_to)
+    today = fields.Date.today()
+    date_from = today - timedelta(days=today.weekday())
+    date_to = date_from + timedelta(weeks=52)
+
+    env['mrp.capacity.week'].sudo().recompute_range(
+        date_from=date_from,
+        date_to=date_to,
+    )
+
+    if 'mrp.capacite.cache' in env:
+        env['mrp.capacite.cache'].sudo().refresh_all(
+            date_from=date_from,
+            date_to=date_to,
+        )
 
 
 class HrLeave(models.Model):
-    _inherit = ['hr.leave', 'mrp.capacity.recompute.mixin']
+    _inherit = 'hr.leave'
 
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
-        records._trigger_capacity_refresh()
+        _trigger_capacity_refresh(self.env)
         return records
 
     def write(self, vals):
         res = super().write(vals)
-        self._trigger_capacity_refresh()
+        _trigger_capacity_refresh(self.env)
         return res
 
     def unlink(self):
         res = super().unlink()
-        self._trigger_capacity_refresh()
+        _trigger_capacity_refresh(self.env)
         return res
 
 
 class ResourceCalendarLeaves(models.Model):
-    _inherit = ['resource.calendar.leaves', 'mrp.capacity.recompute.mixin']
+    _inherit = 'resource.calendar.leaves'
 
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
-        records._trigger_capacity_refresh()
+        _trigger_capacity_refresh(self.env)
         return records
 
     def write(self, vals):
         res = super().write(vals)
-        self._trigger_capacity_refresh()
+        _trigger_capacity_refresh(self.env)
         return res
 
     def unlink(self):
         res = super().unlink()
-        self._trigger_capacity_refresh()
+        _trigger_capacity_refresh(self.env)
         return res
