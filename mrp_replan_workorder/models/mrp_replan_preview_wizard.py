ae091bf491dbabf388d7eacd64ed9494bc9c16af
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-03-18 22:21:25 +0100

    Update

--- mrp_replan_workorder/models/mrp_replan_preview_wizard.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class MrpReplanPreviewWizard(models.TransientModel):
-    _name = "mrp.replan.preview.wizard"
-    _description = "Assistant technique de compatibilité"
-
-    name = fields.Char(default="Compatibilité")
