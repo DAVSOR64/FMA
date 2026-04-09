2bc3b8795bc0d91c2321ae1dd3f527de073848a6
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-04-09 09:40:40 +0200

    Update

--- mrp_fma_planning_unified/__manifest__.py
+++ mrp_fma_planning_unified/__manifest__.py
@@ -4,6 +4,7 @@
     'summary': 'Réordonnancement FMA + recalcul macro batch + popup manuel',
     'depends': ['mrp', 'mail', 'mrp_replan_workorder', 'mrp_replan_workorder_popup', 'purchase'],
     'data': [
+        'security/ir.model.access.csv',
         'views/mrp_production_views.xml',
         'wizards/mrp_recalc_planif_wizard_views.xml',
         'data/ir_cron.xml'
