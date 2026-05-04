6fcbaf9212353f6c4c7c1a86e1cf24b5d29f119f
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-05-04 17:41:43 +0200

    Update

--- product_subfamily/models/__init__.py
+++ product_subfamily/models/__init__.py
@@ -1,2 +1,6 @@
+from . import product_category
+from . import product_family
 from . import product_subfamily
+from . import product_subsubfamily
+from . import product_family_triplet
 from . import product_template
