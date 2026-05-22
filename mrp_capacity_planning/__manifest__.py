9b65d4d5551d1e6dddc73ef29111c62b21725346
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-05-22 16:23:47 +0200

    UPdate

--- mrp_capacity_planning/__manifest__.py
+++ mrp_capacity_planning/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'FMA - Planning & Capacité',
-    'version': '17.0.1.0.0',
+    'version': '17.0.1.0.4',
     'category': 'Manufacturing',
     'summary': 'Planification macro, capacité ressources et réordonnancement FMA',
     'author': 'Paxo Consulting',
--- mrp_capacity_planning/wizard/__init__.py
+++ mrp_capacity_planning/wizard/__init__.py
@@ -2,4 +2,4 @@
 from . import mrp_replan_preview_wizard
 from . import mrp_batch_macro_replan_wizard
 from . import generate_weeks_wizard
-
+from . import mrp_add_component_need_wizard
