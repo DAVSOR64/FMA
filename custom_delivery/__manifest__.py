c2dcd96eb1175b327f16cbee82f9a3ccf2891739
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-06-17 09:40:50 +0200

    Update

---
+++ custom_delivery/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Custom Delivery",
+    "summary": "Custom delivery slip and picking colisage fields",
+    "description": "Adds colisage and palette information on stock pickings and customizes the delivery report.",
+    "author": "Odoo PS",
+    "website": "http://www.yourcompany.com",
+    "category": "Inventory/Delivery",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "base",
+        "stock",
+        "sale_stock",
+        "mail",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock.picking.xml",
+        "views/delivery.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "custom_delivery/static/src/img/lieu.png",
+            "custom_delivery/static/src/img/camion.png",
+            "custom_delivery/static/src/img/appel-telephonique.png",
+            "custom_delivery/static/src/img/enveloppe.png",
+        ],
+    },
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
