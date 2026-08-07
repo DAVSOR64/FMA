5e0725f841771867f9950086520102a8817057ad
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-08-07 15:00:46 +0200

    Vue

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Custom Field Transfer",
-    # 1.0.31 : Chronologie sortie de la colonne de droite, ou elle etait
-    # repoussee en bord d'ecran ; deux dates par ligne.
+    # 1.0.31 : Chronologie sortie de la colonne de droite ou elle etait
+    # repoussee en bord d'ecran. Pleine largeur, deux dates par ligne,
+    # accrochee a l'entete plutot qu'en bas de la fiche.
     "version": "19.0.1.0.31",
     "summary": "Created and Transfer custom field from contact and sale order to invoice",
     "author": "Your Name",
