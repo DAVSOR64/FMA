e47fe286a7913c0e7f54d9ce72327e2c90d12c71
David SORIA <106063065+DAVSOR64@users.noreply.github.com>
2026-08-26 12:38:26 +0200

    Update Delivery Date

--- delivery_service_rate/__manifest__.py
+++ delivery_service_rate/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Delivery Service Rate",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.2.0",
     "author": "Paxo Consulting",
     "category": "Warehouse",
     "summary": "Calcul du taux de service (livraison à temps)",
     "license": "LGPL-3",
-    "depends": ["stock", "sale_management"],
+    "depends": ["stock", "sale_management", "sale_stock"],
     "data": [
         "views/delivery_service_rate_view.xml",
         "views/stock_picking_form_reason.xml",
