5845e5dc8804c09684848f3b5b28951e443ee82d
David SORIA <d.soria@alefredo.com>
2026-08-28 15:18:56 +0200

    Facture : retablir la mise en page personnalisee en v19
    Le bloc affacturage etait ancre sur //div[@class='clearfix'], qui visait
    #payment_term en v17. En v19 ce div porte class="clearfix mt-3" : le match
    exact sur @class ne resolvait plus, l'xpath echouait et toute la vue heritee
    report_invoice_document_inherit etait desactivee a la migration, d'ou le
    retour au PDF standard.
    
    Reancrage sur //div[@id='payment_term']. Les 16 autres xpath du gabarit
    resolvent a l'identique en 17.0 et en 19.0.

--- custom_invoice/__manifest__.py
+++ custom_invoice/__manifest__.py
@@ -3,7 +3,9 @@
     "name": "Custom Invoice Text Block",
     # 1.0.2 : la facture imprimee affiche commercial_id, avec repli sur
     # l'ancienne selection pour les factures anterieures a la bascule.
-    "version": "19.0.1.0.2",
+    # 1.0.3 : xpath du bloc affacturage reancre sur #payment_term, dont la
+    # classe a change en v19 et qui desactivait toute la vue heritee.
+    "version": "19.0.1.0.3",
     "summary": "Show text block on invoice based on contact boolean field",
     "author": "Your Name",
     "depends": ["account", "custom"],
